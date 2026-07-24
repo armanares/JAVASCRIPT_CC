ccd233d5d2575a6b0ae92c50ca75a1752218a8d5
armanares <zaddyarman@gmail.com>
2026-07-24 22:38:11 +0530

    commit

---
+++ 3/01_function.js
@@ -0,0 +1,39 @@
+function calculateResult( val1,val2 , ...num1 ) {
+  return num1 ;
+}
+// ... is rest operator here 
+console.log(calculateResult(5,5,4,3,2))
+
+// 5 , 5 goes in val1 and val2
+
+const user =  {
+  name: "arman" ,
+  roll : 10 
+}
+ // passsing object in a function
+
+function handleObject(anyobject) {
+  console.log(`my name is ${anyobject.name} and my roll is ${anyobject.roll}`)
+  console.log(`my name is ${anyobject.names} and my roll is ${anyobject.roll}`)
+
+}
+
+handleObject(user);
+
+handleObject({
+  name:"arman",
+  roll : 10
+
+})
+
+// passing array
+
+const Array = [200,209 ,208,207]
+
+function returnSecondValue(getArray ){
+  return getArray[1] ;
+}
+
+console.log(returnSecondValue(Array))
+
+
---
+++ 3/02_scope.js
@@ -0,0 +1,30 @@
+let a =90 ;
+
+if(4 ) {
+  const b = 0
+  let a = 6
+  var c = 80 
+  console.log('inner c: ',a)
+
+}
+console.log(a)
+// console.log(b) error
+//console.log(c) accessible 
+
+// scope in console is diff and scope while running code via node in terminal is different
+
+function one(){
+  function two() {
+
+    const dev = "deepti"
+
+    const web = "dev"
+
+    console.log(dev)
+    console.log(web)
+
+  }
+  two()
+}
+
+one()
